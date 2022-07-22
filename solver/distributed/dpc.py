277dcd1779d026b64341de0c13547b922c4044aa
Abdellah <abdellah-elmrini@outlook.com>
2022-07-22 14:31:19 +0200

    Fixed y iteration

--- solver/distributed/dpc.py
+++ solver/distributed/dpc.py
@@ -6,6 +6,7 @@ from scipy.optimize import minimize
 
 from solver.distributed.distributed_solver import DistributedSolver
 from solver.local.sdca import SDCA
+from solver.local.gradient_descent import GD, AGD
  
 
 class Dpc(DistributedSolver):
@@ -24,6 +25,7 @@ class Dpc(DistributedSolver):
             seed= self.seed,
             inner_precision = inner_precision,
             max_inner_repeats = max_inner_repeats ) #TODO fix the local solver parameters 
+        
         self.inner_epochs = inner_epochs
         self.C = self.L_r*self.model.c/2 
         self.alpha = self.step_size*self.model.c/(2 * self.L_g * self.C ) # TODO 
@@ -42,6 +44,6 @@ class Dpc(DistributedSolver):
         C = self.C
         a = (eta/C ) * (self.gr_F - eta/(sigma-eta*self.alpha) *self.omega + (sigma - eta*(1+self.alpha))/(1-eta*self.alpha/sigma) * self.y)
         self.x = self.local_solver.run_step(a,self.inner_epochs) #TODO 
-        self.y -= eta*(self.x + (self.y + self.omega)/self.model.c)
-        self.omega -= eta*self.communication_step(self.omega+self.y)
+        self.y = 1/(1-eta*self.alpha/sigma) * ((sigma- eta*(1+self.alpha))/sigma * self.y - eta/sigma*self.omega+eta*self.x )
+        self.omega -= eta/sigma*self.communication_step(self.omega+self.y)
        
---
+++ solver/local/gradient_descent.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from solver.solver import Solver
+
+
+class GD(Solver):
+    def __init__(self, **kwargs):
+        super(GD, self).__init__(**kwargs)
+        self.L = self.model.get_L(self.dataset)
+        self.set_regularization(self.model.c)
+
+    def set_regularization(self, lam):
+        self.step_size = 1. / (self.L + lam) 
+        self.lam = lam
+
+    def run_step(self, grad, nb_iters):
+        for _ in range(nb_iters):
+            self.x = (1 - self.step_size * self.lam) * self.x - self.step_size * (self.model.get_gradient(self.x, self.dataset) + grad)
+        self.subproblem_grad_norm = np.linalg.norm(self.model.get_gradient(self.x, self.dataset) + grad + (self.lam - self.model.c) * self.x)
+        return np.copy(self.x)
+
+class AGD(Solver):   
+    def __init__(self, **kwargs):
+        super(AGD, self).__init__(**kwargs)
+        self.lam = self.model.c
+        self.L = self.model.get_L(self.dataset)
+        self.set_regularization(self.model.c)
+        self.y = np.copy(self.x)
+        self.subproblem_grad_norm = 0.
+
+    def set_regularization(self, lam):
+        self.step_size = 1. / (self.L + lam) 
+        sqrt_kappa = np.sqrt(1. / (self.step_size * lam))
+        self.beta = (sqrt_kappa - 1) / (sqrt_kappa + 1)
+        self.lam = lam
+
+    def run_step(self, grad, nb_epochs):
+        self.subproblem_grad_norm = np.inf
+        inner_repeats_count = 0
+        while inner_repeats_count < self.max_inner_repeats:
+            if self.subproblem_grad_norm < self.inner_precision:
+                break 
+            for _ in range(nb_epochs):
+                subproblem_grad = self.model.get_gradient(self.x, self.dataset) + grad + (self.lam - self.model.c) * self.x
+                y_new = self.x - self.step_size * subproblem_grad 
+                self.x = (1 + self.beta) * y_new - self.beta * self.y 
+                self.y = y_new
+
+            self.subproblem_grad_norm = np.linalg.norm(subproblem_grad)
+            inner_repeats_count += 1
+
+        # Safer to copy (although it is more expensive)
+        return np.copy(self.x)
